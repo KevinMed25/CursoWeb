e11a9426a37d32444690c2490217a0d4b1a27556
Alejandro Medina <kevinmedina2509Jr@gmail.com>
2023-04-26 00:53:58 -0600

    video # 109 objeto math

---
+++ Introducción_JS/js/objeto-math.js
@@ -0,0 +1,40 @@
+// Objeto Math
+
+console.log(Math.random()); //numero aleatorio
+console.log(Math.random()); //numero aleatorio
+
+let resultado;
+resultado = Math.PI;
+console.log(resultado);
+// Math.round aplica criterio .5 arriba menor .5 abajo
+resultado = Math.round(2.56646);
+console.log(resultado);
+resultado = Math.round(2.26646);
+console.log(resultado);
+
+// funcion techo
+resultado = Math.ceil(4.5);
+console.log(resultado);
+
+//funcion piso:
+resultado = Math.floor(4.5);
+console.log(resultado);
+
+//raiz
+resultado = Math.sqrt(144);
+console.log(resultado);
+
+// valor absoluto
+resultado = Math.abs(-2);
+console.log(resultado);
+
+// retorna minimo entre todos
+resultado = Math.min(1,2,3);
+console.log(resultado);
+// retorna máximo
+resultado = Math.max(1,2,3);
+console.log(resultado);
+// generar un aleatorio en cierto rango
+resultado = Math.floor(Math.random()*11); //0-11
+console.log(resultado);
+ 
