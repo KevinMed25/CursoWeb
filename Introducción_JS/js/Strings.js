694d2640075624c3628da932dc6dff3ca1dbb655
Alejandro Medina <kevinmedina2509Jr@gmail.com>
2023-04-26 00:08:33 -0600

    video #106 - Strings

---
+++ Introducción_JS/js/Strings.js
@@ -0,0 +1,20 @@
+// String -- cadena de texto
+
+const producto = "monitor"; // Variable tipo string
+console.log(producto);
+
+//Otra forma de crear un string
+const producto2 = String("monitor");
+console.log(producto2);
+
+const producto3 = new String("monitor"); //lo crea como objeto
+console.log(producto3);
+
+// typeof muestra el tipo de dato
+console.log(typeof producto1); 
+console.log(typeof producto2); 
+console.log(typeof producto3); 
+//Nota, no mezclar comillas dobles con sencillas xd
+
+const salidaCaracter = "string con \"comillas\"";
+console.log(salidaCaracter);
