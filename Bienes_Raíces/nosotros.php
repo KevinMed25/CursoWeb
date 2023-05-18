165e3766b2c2b42578c2f52766312e4900efabad
Alejandro Medina <kevinmedina2509Jr@gmail.com>
2023-05-18 01:25:11 -0600

    Seccion 28 y 29 lista

--- Bienes_Raíces/nosotros.php
+++ Bienes_Raíces/nosotros.php
@@ -1,39 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bienes Raíces</title>
-    <link rel="stylesheet" href="build/css/app.css">
-</head>
-<body>
-    <header class="header">
-        <div class=" contenedor contenido-header">
-            <div class="barra">
-                <a href="index.html">
-                    <img src="build/img/logo.svg" alt="img logo bienes raíces">
-                </a>
-                <!-- menu -->
-                <div class="mobile-menu">
-                    <img src="build/img/barras.svg" alt="menu">
-                </div>
+<?php
+    require 'includes/funciones.php';
+    incluirTemplate('header');
+?>
 
-                <!-- darkmode -->
-                <div class="derecha">
-                    <img class="dark-mode-boton" src="build/img/dark-mode.svg" alt="dark mode">
-                    <nav class="navegacion">
-                        <a href="nosotros.html">Nosotros</a>
-                        <a href="anuncios.html">Anuncios</a>
-                        <a href="blog.html">Blog</a>
-                        <a href="contacto.html">Contacto</a>
-                    </nav>
-                </div>
-            </div>
-            <!-- <h1>Venta de Casas y Departamentos Exclusivos de Lujo</h1> -->
-        </div>
-        
-    </header>
     <main class="contenedor">
         <h1 class="negritas">Conoce Sobre Nosotros</h1>
         <div class="contenido-nosotros">
@@ -84,17 +53,7 @@
             </div>
         </div>
     </section>
-    <footer class=" footer seccion">
-        <div class="contenedor contenedor-footer">
-            <nav class="navegacion">
-                <a href="nosotros.html">Nosotros</a>
-                <a href="anuncios.html">Anuncios</a>
-                <a href="blog.html">Blog</a>
-                <a href="contacto.html">Contacto</a>
-            </nav>
-            <p class="copyright"> Todos los Derechos Reservados 2021 &copy;</p>
-        </div>
-    </footer>
-    <script src="build/js/bundle.min.js"></script>
-</body>
-</html>
+
+<?php 
+    incluirTemplate('footer');
+?>
