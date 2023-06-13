dd33b9dfa5379402872bb43df627cede1a09acae
Alejandro Medina <kevinmedina2509Jr@gmail.com>
2023-06-13 00:32:33 -0600

    Correccion enlace roto

--- Bienes_Raices_MVC/views/layout.php
+++ Bienes_Raices_MVC/views/layout.php
@@ -64,7 +64,7 @@
         <div class="contenedor contenedor-footer">
             <nav class="navegacion">
                 <a href="/nosotros">Nosotros</a>
-                <a href="/anuncios">Anuncios</a>
+                <a href="/propiedades">Anuncios</a>
                 <a href="/blog">Blog</a>
                 <a href="/contacto">Contacto</a>
             </nav>
