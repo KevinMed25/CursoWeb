620f3a9b29b438a0066e2c9676d5a9455c86e7f7
Alejandro Medina <kevinmedina2509Jr@gmail.com>
2023-06-14 20:54:04 -0600

    seccion 45 lista

--- AppSalon_MVC/public/index.php
+++ AppSalon_MVC/public/index.php
@@ -2,12 +2,24 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\LoginController;
 use MVC\Router;
 
 $router = new Router();
 
+//inicio de sesion:
+$router->get("/",[LoginController::class, 'login']);
+$router->post("/",[LoginController::class, 'login']);
+$router->post("/logout",[LoginController::class, 'logout']);
 
+//recuperar password
+$router->get("/olvide",[LoginController::class, 'olvide']);
+$router->post("/olvide",[LoginController::class, 'olvide']);
+$router->get("/recuperar",[LoginController::class, 'recuperar']);
+$router->post("/recuperar",[LoginController::class, 'recuperar']);
 
+//crear cuenta
+$router->get("/crearCuenta",[LoginController::class, 'crear']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
---
+++ AppSalon_MVC/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'kevin/app-salon_mvc',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '860d9fbc011dcf7a9fdeb8baf1817cd3abeb23fc',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'kevin/app-salon_mvc' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '860d9fbc011dcf7a9fdeb8baf1817cd3abeb23fc',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
