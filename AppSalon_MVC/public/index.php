60678512070354a7670c7148e27a758969d04e3c
Alejandro Medina <kevinmedina2509Jr@gmail.com>
2023-06-22 00:44:42 -0600

    Seccion 52

--- AppSalon_MVC/public/index.php
+++ AppSalon_MVC/public/index.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\CitaController;
 use Controllers\LoginController;
 use MVC\Router;
 
@@ -25,6 +26,9 @@ $router->post("/crearCuenta",[LoginController::class, 'crear']);
 //para confirmar cuenta:
 $router->get("/confirmarCuenta",[LoginController::class, 'confirmar']);
 $router->get("/mensaje",[LoginController::class, 'mensaje']);
+
+//privado:
+$router->get('/cita', [CitaController::class, 'index']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
