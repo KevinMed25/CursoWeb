26afa07d6bfbfcadbc69d41a75ecf3f66dcacbf6
Alejandro Medina <kevinmedina2509Jr@gmail.com>
2023-06-24 02:44:04 -0600

    Seccion 53 video #505

--- AppSalon_MVC/src/js/app.js
+++ AppSalon_MVC/src/js/app.js
@@ -2,6 +2,13 @@ let paso = 1;
 let pasoInicial = 1;
 let pasoFinal = 3;
 
+const cita = {
+    nombre: '',
+    fecha: '',
+    hora: '',
+    servicios: [],
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     iniciarApp();
 });
@@ -12,6 +19,8 @@ function iniciarApp() {
     tabs(); //cambio de seccion
     paginaSiguiente();
     paginaAnterior();
+
+    consultarAPI();//consultar api en backend php
 }
 
 function mostrarSeccion(){
@@ -81,4 +90,60 @@ function paginaSiguiente() {
         paso++;
         botonesPaginador();
     });
+}
+
+async function consultarAPI() {
+    try {
+        const url = '/api/servicios';
+        const resultado = await fetch(url);
+        const servicios = await resultado.json();
+        mostrarServicios(servicios);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function mostrarServicios(servicios) {
+    servicios.forEach(servicio => {
+        const { id, nombre, precio } = servicio;
+        // console.log(id);
+        
+        const nombreServicio = document.createElement('p');
+        nombreServicio.classList.add('nombre-servicio');
+        nombreServicio.textContent= nombre;
+
+        const precioServicio  = document.createElement('P');
+        precioServicio.classList.add('precio-servicio');
+        precioServicio.textContent = `$${precio}`;
+
+        const servicioDiv = document.createElement('DIV');
+        servicioDiv.classList.add('servicio');
+        servicioDiv.dataset.idServicio = id;
+        servicioDiv.onclick = function() {
+            seleccionarServicio(servicio);
+        };
+
+        servicioDiv.appendChild(nombreServicio);
+        servicioDiv.appendChild(precioServicio);
+
+        document.querySelector('#servicios').appendChild(servicioDiv);
+        
+    });
+}
+
+function seleccionarServicio(servicio) {
+    const {id} = servicio;
+    const {servicios} = cita;
+    const divServicio = document.querySelector(`[data-id-servicio="${id}"]`);//identifica al elemento que al que se da click
+
+    //para comprobar si el servicio ya se ha agregado
+    if(servicios.some( agregado => agregado.id === id)) {
+        //eliminarlo
+        cita.servicios = servicios.filter(agregado => agregado.id !== id);
+        divServicio.classList.remove('seleccionado');
+    } else { //aregarlo
+        cita.servicios = [...servicios, servicio];
+        divServicio.classList.add('seleccionado');
+    } 
+    console.log(servicio);
 }
